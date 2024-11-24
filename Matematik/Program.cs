d5b2f25722ef6b9abd5cfa2edbd7a5047b54084c
Monster <Monster@DESKTOP-SR3QA4U>
2024-11-24 23:08:25 +0300

    Add project files.

---
+++ Matematik/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Matematik;
+
+DortIslem dortIslem = new DortIslem();
+dortIslem.Topla(4, 5);
+dortIslem.Topla(6, 9);
+
+
+
+
